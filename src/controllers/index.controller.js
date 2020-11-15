b39a9d6e3fd584d4831618dafc282dc49797c91e
wildercossio <cwilrodz@gmail.com>
2020-11-14 20:02:09 -0400

    conecion a la bd postgres de heroku

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,22 +1,30 @@
-const { Pool } = require("pg");
+const { Client } = require("pg");
 
-const database = new Pool({
-  host: "ec2-54-164-134-207.compute-1.amazonaws.com",
-  user: "ucralauolpfuxc",
-  password: "38afec102b9182a320e2c9afe515a1b6723d307eda29dc7f175037cb693b9649",
-  database: "d8god16o1u8tk"
+const database = new Client({
+  connectionString: "postgres://ucralauolpfuxc:38afec102b9182a320e2c9afe515a1b6723d307eda29dc7f175037cb693b9649@ec2-54-164-134-207.compute-1.amazonaws.com:5432/d8god16o1u8tk",
+  ssl: { rejectUnauthorized: false }
 });
 
+database.connect();
+
 const getTorneos = async (req, res) => {
-  const response = await database.query("select * from torneos");
-  console.log(response.rows);
-  res.status(200).json(response.rows);
+  try {
+    const response = await database.query("select * from torneos");
+    console.log(response.rows);
+    res.status(200).json(response.rows);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const getCategorias = async (req, res) => {
-  const response = await database.query("select * from categoria");
-  console.log(response.rows);
-  res.status(200).json(response.rows);
+  try {
+    const response = await database.query("select * from categoria");
+    console.log(response.rows);
+    res.status(200).json(response.rows);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {
