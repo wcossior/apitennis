cdcfc5e31b0a9991d6a1eb7466e91495ada86615
wildercossio <cwilrodz@gmail.com>
2021-04-25 17:29:57 -0400

    sets y obtener un torneo

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -19,6 +19,46 @@ const getTorneos = async (req, res) => {
   }
 }
 
+const getTorneo = async (req, res) => {
+  try {
+
+    //obteniendo la categoria del jugador de su ultima inscripcion
+    const text = "select max(fecha_inscripcion), categorium_id from jugadors where ci=$1 group by categorium_id";
+    const values = [req.params.ci];
+    const response = await database.query(text, values);
+    if (!response.rows[0]) {
+      res.status(200).json([]);
+    }
+    const data = response.rows[0];
+    const idCategoria = [data.categorium_id];
+
+    const text2 = "select torneo_id from categoria where id=$1";
+    const response2 = await database.query(text2, idCategoria);
+    const data2 = response2.rows[0];
+    const idTorneo = [data2.torneo_id];
+
+
+    const text3 = "select * from torneos where id=$1";
+    const response3 = await database.query(text3, idTorneo);
+    var today = new Date();
+    var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    const dateTorneo = response3.rows[0].fecha_fin;
+    var finishDateTorneo = dateTorneo.getFullYear() + '-' + (dateTorneo.getMonth() + 1) + '-' + dateTorneo.getDate();
+
+    //si el torneo ya termino no retorna nada
+    if (finishDateTorneo > currentDate) {
+      res.status(200).json(response3.rows);
+    } else {
+      res.status(200).json([]);
+    }
+  } catch (error) {
+    res.status(500).send({ msg: "Ocurrio un error" });
+  }
+}
+
+const newMessage = async (req, res) => {
+  
+}
 const getCategorias = async (req, res) => {
   try {
     const text = "select * from categoria where torneo_id=$1";
@@ -51,9 +91,7 @@ const getCuadros = async (req, res) => {
     res.status(500).send({ msg: "Ocurrio un error" });
   }
 }
-// select etapa, hora_inicio, jugadors.nombre as jug1 from partidos, cuadros, jugadors where partidos.cuadro_id=cuadros.id and jugadors.id=partidos.jugador_uno_id and cuadros.categorium_id=$1
-// select partidos.id, partidos.jugador_uno_id, partidos.jugador_dos_id, jugadores1.nombre as jug1, jugadores2.nombre as jug2 from partidos inner join jugadors jugadores1 on jugadores1.id=partidos.jugador_uno_id inner join jugadors jugadores2 on jugadores2.id=partidos.jugador_dos_id
-//select etapa, hora_inicio, jugadores1.nombre as jug1, jugadores2.nombre as jug2 from partidos, cuadros, jugadors jugadores1, jugadors jugadores2 where partidos.cuadro_id=cuadros.id and jugadores1.id=partidos.jugador_uno_id and jugadores2.id=partidos.jugador_dos_id and cuadros.categorium_id=$1 
+
 const getPartidosCuadro = async (req, res) => {
   try {
     const text = "select partidos.id, partidos.numero_cancha, score_jug1_set, score_jug2_set, etapa, score_jugador1, score_jugador2, hora_inicio, partidos.jugador_uno_id, partidos.jugador_dos_id, jugadores1.nombre as jug1, jugadores2.nombre as jug2 from partidos, cuadros, jugadors jugadores1, jugadors jugadores2 where partidos.cuadro_id=cuadros.id and jugadores1.id=partidos.jugador_uno_id and jugadores2.id=partidos.jugador_dos_id and cuadros.categorium_id=$1 ";
@@ -64,11 +102,52 @@ const getPartidosCuadro = async (req, res) => {
     res.status(500).send({ msg: "Ocurrio un error" });
   }
 }
-//select * from partidos, grupos where partidos.grupo_id=grupos.id and grupos.categorium_id=$1
+const getSets = async (req, res) => {
+  try {
+    const text = "select * from sets where id_partido=$1";
+    const values = [req.params.id];
+    const response = await database.query(text, values);
+    res.status(200).json(response.rows);
+  } catch (error) {
+    res.status(500).send({ msg: "Ocurrio un error" });
+  }
+}
+
+const newSet = async (req, res) => {
+  try {
+    const text = "insert into sets values ($1, $2, $3, $4, $5)";
+    var id = new Date().valueOf();
+    const partidoId = parseInt([req.body.idPartido]);
+    const scoreJug1 = parseInt([req.body.scoreJug1]);
+    const scoreJug2 = parseInt([req.body.scoreJug2]);
+    const nroSet = parseInt([req.body.nroSet]);
+
+    await database.query(text, [id, partidoId, scoreJug1, scoreJug2, nroSet]);
+    res.status(200).json("Score guardado");
+  } catch (error) {
+    res.status(500).send({ msg: "Ocurrio un error"+error });
+  }
+}
+
+const updateSet = async (req, res) => {
+  try {
+    const text = "update sets set score_jug_1=$1, score_jug_2=$2, id_partido=$3, nro_set=$4 where id=$5";
+    const partidoId = parseInt([req.body.idPartido]);
+    const scoreJug1 = parseInt([req.body.scoreJug1]);
+    const scoreJug2 = parseInt([req.body.scoreJug2]);
+    const nroSet = parseInt([req.body.nroSet]);
+    const idSet = parseInt([req.body.idSet]);
+
+    const response = await database.query(text, [scoreJug1, scoreJug2, partidoId, nroSet, idSet]);
+    res.status(200).json("Score actualizado");
+  } catch (error) {
+    res.status(500).send({ msg: "Ocurrio un error" });
+  }
+}
 
 const getPartidosGrupo = async (req, res) => {
   try {
-    const text = "select partidos.id, partidos.numero_cancha, grupos.nombre, score_jug1_set, score_jug2_set, score_jugador1, score_jugador2, hora_inicio, partidos.jugador_uno_id, partidos.jugador_dos_id, jugadores1.nombre as jug1, jugadores2.nombre as jug2 from partidos, grupos, jugadors jugadores1, jugadors jugadores2 where partidos.grupo_id=grupos.id and jugadores1.id=partidos.jugador_uno_id and jugadores2.id=partidos.jugador_dos_id and grupos.categorium_id=$1 ";
+    const text = "select partidos.id, partidos.numero_cancha, grupos.nombre, score_jugador1, score_jugador2, hora_inicio, partidos.jugador_uno_id, partidos.jugador_dos_id, jugadores1.nombre as jug1, jugadores2.nombre as jug2 from partidos, grupos, jugadors jugadores1, jugadors jugadores2 where partidos.grupo_id=grupos.id and jugadores1.id=partidos.jugador_uno_id and jugadores2.id=partidos.jugador_dos_id and grupos.categorium_id=$1 ";
     const values = [req.params.id];
     const response = await database.query(text, values);
     res.status(200).json(response.rows);
@@ -93,60 +172,33 @@ const updatePartidos = async (req, res) => {
 
 const newUser = async (req, res) => {
   try {
+    var id = new Date().valueOf();
     const ci = req.body.ci;
     const nombre = req.body.nombre;
     const email = req.body.email;
     var password = req.body.password;
-    var text = "select * from users where email=$1";
+    var text = "select * from usuarios where email=$1";
     var response = await database.query(text, [email]);
 
     if (response.rows.length != 0) {
       res.status(400).json({ msg: "Este email ya esta en uso" });
     } else {
-      var text = "insert into users values ($1, $2, $3, $4, $5, $6)";
+      var text = "insert into usuarios values ($1, $2, $3, $4, $5, $6)";
       password = await bcrypt.hash(password, 10);
-      var response = await database.query(text, [ci, nombre, email, password, 1, "Jugador"]);
+      var response = await database.query(text, [id, ci, nombre, email, password, "Jugador"]);
       res.status(200).json({ msg: "Cuenta creada exitosamente!" });
     }
   } catch (e) {
-    res.status(500).send({ msg: "Ocurrio un error" });
+    res.status(500).send({ msg: "Ocurrio un error" + e });
   }
 }
 
-
-// const login = async (req, res) => {
-//   try {
-//     const user = req.body.user;
-//     const passId = req.body.password;
-//     console.log(user + " " + passId);
-
-//     var text = "select * from users where (nombre=$1 or email=$1) and state=1";
-//     var response = await database.query(text, [user]);
-//     if (response.rows.length != 0) {
-
-//       const text = `select * from users where (nombre=$1 or email=$1) and password=$2`;
-//       const datos = await database.query(text, [user, passId]);
-
-//       if (datos.rows.length != 0) {
-//         res.status(200).json(datos.rows[0]);
-//       } else {
-//         res.status(404).send({ msg: "Nombre o constraseña incorrectos" });
-//       }
-
-//     } else {
-//       res.status(404).send({ msg: "El usuario no existe" });
-//     }
-
-//   } catch (e) {
-//     res.status(500).send({ msg: "Ocurrio un error" });
-//   }
-// }
 const login = async (req, res) => {
   try {
     const email = req.body.email;
     const passId = req.body.password;
 
-    var text = "select * from users where email=$1 and state=1";
+    var text = "select * from usuarios where email=$1";
     var response = await database.query(text, [email]);
     var user = response.rows[0];
 
@@ -173,7 +225,7 @@ const login = async (req, res) => {
 
 const getProg = async (req, res) => {
   try {
-    const text = "select * from programacion where torneo_id=$1";
+    const text = "select * from programacion where torneo_id=$1 ";
     const values = [req.params.id];
     const response = await database.query(text, values);
     res.status(200).json(response.rows);
@@ -192,5 +244,10 @@ module.exports = {
   updatePartidos,
   newUser,
   login,
-  getProg
+  getProg,
+  getTorneo,
+  newMessage,
+  getSets,
+  newSet,
+  updateSet,
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,22 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getTorneos, getCategorias, getCuadros, getGrupos, getPartidosCuadro, getPartidosGrupo, updatePartidos, login, newUser, getProg } = require("../controllers/index.controller");
+const { getTorneos, getCategorias, getCuadros, getGrupos, getPartidosCuadro, getPartidosGrupo, updatePartidos, login, newUser, getProg, getTorneo, newMessage, getSets, newSet, updateSet } = require("../controllers/index.controller");
 
 router.get("/torneos", getTorneos);
+router.get("/torneo/:ci", getTorneo);
 router.get("/programacion/:id", getProg);
 router.get("/categorias/:id", getCategorias);
 router.get("/cuadros/:id", getCuadros);
 router.get("/grupos/:id", getGrupos);
 router.get("/partidoscuadro/:id", getPartidosCuadro);
+router.get("/sets/:id", getSets);
 router.get("/partidosgrupo/:id", getPartidosGrupo);
 router.put("/partidos/:id/:scoreJug1/:scoreJug2", updatePartidos);
 router.post("/login", login);
 router.post("/newUser", newUser);
+router.post("/newMessage", newMessage);
+router.post("/newSet", newSet);
+router.put("/updateSet", updateSet);
 
 module.exports = router;
