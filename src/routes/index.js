afb7d7ad4eded2f2569c3d3dc7ca6fb4c77c5ec6
Wilder Cossio <w.cossio.r@gmail.com>
2021-06-01 11:59:21 -0400

    mensajes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getTorneos, getCategorias, getCuadros, getGrupos, getPartidosCuadro, getPartidosGrupo, updatePartidos, login, newUser, getProg, getTorneo, newMessage, getSets, newSet, updateSet, deleteSet } = require("../controllers/index.controller");
+const { getTorneos, getCategorias, getCuadros, getGrupos, getPartidosCuadro, getPartidosGrupo, updatePartidos, login, newUser, getProg, getTorneo, sendMessage, getSets, newSet, updateSet, deleteSet,getNotifications } = require("../controllers/index.controller");
 
 router.get("/torneos", getTorneos);
+router.get("/notifications", getNotifications);
 router.get("/torneo/:ci", getTorneo);
 router.get("/programacion/:id", getProg);
 router.get("/categorias/:id", getCategorias);
@@ -15,7 +16,7 @@ router.get("/partidosgrupo/:id", getPartidosGrupo);
 router.put("/partidos/:id/:scoreJug1/:scoreJug2", updatePartidos);
 router.post("/login", login);
 router.post("/newUser", newUser);
-router.post("/newMessage", newMessage);
+router.post("/sendMessage", sendMessage);
 router.post("/newSet", newSet);
 router.put("/updateSet", updateSet);
 router.delete("/deleteSet/:id", deleteSet);
